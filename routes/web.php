10add075879a08342ba227f3a69d835cbbeef617
stenlyysamberi <samstenly815@gmail.com>
2024-04-16 16:37:35 +0800

    middleware login

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('dash', function () {
     // halaman dashboard
-})->middleware('auth')->middleware('auth');;
+})->middleware('auth')->middleware('auth');
 
 Route::get('/', function () {
     return view('html.logins')->name('login');
@@ -20,44 +20,44 @@ Route::get('/', function () {
 Route::get('logout', [ControllerUser::class, 'logout'])->name('logout');
 
 Route::prefix('login')->name('login.')->group(function () {
-    Route::get('/', [ControllerUser::class, 'index'])->name('index')->middleware('auth');;
-    Route::post('/', [ControllerUser::class, 'auth'])->name('auth')->middleware('auth');;
+    Route::get('/', [ControllerUser::class, 'index'])->name('index');
+    Route::post('/', [ControllerUser::class, 'auth'])->name('auth');
 });
 
 Route::prefix('dash')->name('dash.')->group(function () {
-    Route::get('/', [ControllerDashboard::class, 'index'])->name('index')->middleware('auth');;
-    Route::post('/', [ControllerDashboard::class, 'store'])->name('store')->middleware('auth');;
-    Route::delete('/{id}', [ControllerDashboard::class, 'destroy'])->name('destroy')->middleware('auth');;
+    Route::get('/', [ControllerDashboard::class, 'index'])->name('index')->middleware('auth');
+    Route::post('/', [ControllerDashboard::class, 'store'])->name('store')->middleware('auth');
+    Route::delete('/{id}', [ControllerDashboard::class, 'destroy'])->name('destroy')->middleware('auth');
 });
 
 Route::prefix('peserta')->name('peserta.')->group(function () {
-    Route::get('/', [ControllerPeserta::class, 'index'])->name('index')->middleware('auth');;
-    Route::post('/', [ControllerPeserta::class, 'store'])->name('store')->middleware('auth');;
-    Route::delete('/{id}', [ControllerPeserta::class, 'destroy'])->name('destroy')->middleware('auth');;
+    Route::get('/', [ControllerPeserta::class, 'index'])->name('index')->middleware('auth');
+    Route::post('/', [ControllerPeserta::class, 'store'])->name('store')->middleware('auth');
+    Route::delete('/{id}', [ControllerPeserta::class, 'destroy'])->name('destroy')->middleware('auth');
 });
 
 Route::prefix('kategori_lomba')->name('kategori_lomba.')->group(function () {
-    Route::get('/', [ControllerKategoriLomba::class, 'index'])->name('index')->middleware('auth');;
-    Route::post('/', [ControllerKategoriLomba::class, 'store'])->name('store')->middleware('auth');;
-    Route::delete('/{id}', [ControllerKategoriLomba::class, 'destroy'])->name('destroy')->middleware('auth');;
+    Route::get('/', [ControllerKategoriLomba::class, 'index'])->name('index')->middleware('auth');
+    Route::post('/', [ControllerKategoriLomba::class, 'store'])->name('store')->middleware('auth');
+    Route::delete('/{id}', [ControllerKategoriLomba::class, 'destroy'])->name('destroy')->middleware('auth');
 });
 
 Route::prefix('data_jemaat')->name('data_jemaat.')->group(function () {
-    Route::get('/', [ControllerDataJemaat::class, 'index'])->name('index')->middleware('auth');;
-    Route::post('/', [ControllerDataJemaat::class, 'store'])->name('store')->middleware('auth');;
-    Route::delete('/{id}', [ControllerDataJemaat::class, 'destroy'])->name('destroy')->middleware('auth');;
+    Route::get('/', [ControllerDataJemaat::class, 'index'])->name('index')->middleware('auth');
+    Route::post('/', [ControllerDataJemaat::class, 'store'])->name('store')->middleware('auth');
+    Route::delete('/{id}', [ControllerDataJemaat::class, 'destroy'])->name('destroy')->middleware('auth');
 });
 
 Route::prefix('kategori_jemaat')->name('kategori_jemaat.')->group(function () {
-    Route::get('/', [ControllerKategoriJemaat::class, 'index'])->name('index')->middleware('auth');;
-    Route::post('/', [ControllerKategoriJemaat::class, 'store'])->name('store')->middleware('auth');;
-    Route::delete('/{id}', [ControllerKategoriJemaat::class, 'destroy'])->name('destroy')->middleware('auth');;
+    Route::get('/', [ControllerKategoriJemaat::class, 'index'])->name('index')->middleware('auth');
+    Route::post('/', [ControllerKategoriJemaat::class, 'store'])->name('store')->middleware('auth');
+    Route::delete('/{id}', [ControllerKategoriJemaat::class, 'destroy'])->name('destroy')->middleware('auth');
 });
 
 Route::prefix('genre_lagu')->name('genre_lagu.')->group(function () {
-    Route::get('/', [ControllerGenreLagu::class, 'index'])->name('index')->middleware('auth');;
-    Route::post('/', [ControllerGenreLagu::class, 'store'])->name('store')->middleware('auth');;
-    Route::delete('/{id}', [ControllerGenreLagu::class, 'destroy'])->name('destroy')->middleware('auth');;
+    Route::get('/', [ControllerGenreLagu::class, 'index'])->name('index')->middleware('auth');
+    Route::post('/', [ControllerGenreLagu::class, 'store'])->name('store')->middleware('auth');
+    Route::delete('/{id}', [ControllerGenreLagu::class, 'destroy'])->name('destroy')->middleware('auth');
 });
 
 
