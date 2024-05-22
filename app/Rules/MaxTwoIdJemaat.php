91d21cf7c51dfec3390256871e2788882359f5fe
stenlyysamberi <samstenly815@gmail.com>
2024-05-22 22:21:34 +0800

    maxinput validasi

--- app/Rules/MaxTwoIdJemaat.php
+++ app/Rules/MaxTwoIdJemaat.php
@@ -4,9 +4,13 @@ namespace App\Rules;
 
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Support\Facades\DB;
 
 class MaxTwoIdJemaat implements ValidationRule
 {
+
+    protected $idJemaat;
+
     /**
      * Run the validation rule.
      *
@@ -14,6 +18,27 @@ class MaxTwoIdJemaat implements ValidationRule
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        //
+        
+    }
+
+    public function __construct($idJemaat)
+    {
+        $this->idJemaat = $idJemaat;
+    }
+
+    public function passes($attribute, $value)
+    {
+        // Hitung jumlah entri dengan id_jemaat yang sama
+        $count = DB::table('tbl_register')
+                    ->where('id_jemaat', $this->idJemaat)
+                    ->count();
+
+        // Pastikan tidak lebih dari dua
+        return $count < 2;
+    }
+
+    public function message()
+    {
+        return 'Id Jemaat tidak boleh lebih dari dua entri yang sama.';
     }
 }
