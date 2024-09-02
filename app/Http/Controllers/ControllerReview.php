570ebbe385841e9bc863789e37308923f6412a44
samberi.stenly <samberi.stenly@gmail.com>
2024-09-02 11:04:24 +0000

    hddsahjd

--- app/Http/Controllers/ControllerReview.php
+++ app/Http/Controllers/ControllerReview.php
@@ -122,7 +122,7 @@ class ControllerReview extends Controller
         
         $groupedReviews = $groupedReviews->map(function ($userReviews) {
         $totalFinal = 0;
-        $nilai_akhir = 0;
+        double $nilai_akhir = 0;
         $medali = "";
         
         $mappedReviews = $userReviews->map(function ($reviews) use (&$totalFinal) {
