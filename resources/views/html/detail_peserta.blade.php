3362a9ba77021bc8297c2bdb68c91a6af70d1d87
samberi.stenly <samberi.stenly@gmail.com>
2024-09-23 00:14:00 +0000

    sedsadadadsds

--- resources/views/html/detail_peserta.blade.php
+++ resources/views/html/detail_peserta.blade.php
@@ -8,57 +8,57 @@
       <div class="card">
         <div class="card-body">
           @include('alert.message')
-          <form action="{{ route('peserta.updated_peserta', $peserta[0]->idr) }}" method="POST">
-            @csrf
-            @method('PUT')
-              <div class="mb-3 col-3">
-                <label for="exampleInputEmail1" class="form-label">Kordinator</label>
-                <input name="kordinator" type="text" value="{{ $peserta[0]->kordinator }}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-              </div>
+         
+<form action="{{ route('peserta.updated_peserta', $peserta[0]->idr) }}" method="POST">
+    @csrf
+    @method('PUT')
+    <div class="horizontal">
+        <div class="mb-3 col-6">
+            <label for="exampleInputEmail1" class="form-label">Nama Jemaat</label>
+            <input name="nama_jemaat" type="text" value="{{ $peserta[0]->nama_jemaat }}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
 
-              <div class="mb-3 col-3">
-                <label for="exampleInputEmail1" class="form-label">Phone</label>
-                <input name="phone" type="text" value="{{ $peserta[0]->phone }}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-               
-              </div>
-            </div>
+        <div class="mb-3 col-3">
+            <label for="exampleInputEmail1" class="form-label">Kordinator</label>
+            <input name="kordinator" type="text" value="{{ $peserta[0]->kordinator }}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
 
-            <div class="horizontal">
-              <div class="mb-3 col-6">
-                <label for="exampleInputEmail1" class="form-label">Lagu Wajib</label>
-                <input name="lagu_wajib" type="text" value="{{ $peserta[0]->lagu_wajib }}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                
-              </div>
-  
-              <div class="mb-3 col-3">
-                <label for="exampleInputEmail1" class="form-label">Lagu Pilihan</label>
-                <input name="lagu_pilihan" type="text" value="{{ $peserta[0]->judul_lagu }}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                
-              </div>
+        <div class="mb-3 col-3">
+            <label for="exampleInputEmail1" class="form-label">Phone</label>
+            <input name="phone" type="text" value="{{ $peserta[0]->phone }}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+    </div>
 
-              <div class="mb-3 col-3">
-                <label for="exampleInputEmail1" class="form-label">Kategori Lomba</label>
-                <input name="kategori_lomba" type="text" value="{{ $peserta[0]->kategori_lomba }}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                
-              </div>
-            </div>
+    <div class="horizontal">
+        <div class="mb-3 col-6">
+            <label for="exampleInputEmail1" class="form-label">Lagu Wajib</label>
+            <input name="lagu_wajib" type="text" value="{{ $peserta[0]->lagu_wajib }}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
 
-            <div class="horizontal">
-              <div class="mb-3 col-9">
-                <label for="exampleInputEmail1" class="form-label">Tanggal Pendaftaran</label>
-                <input disabled name="tgl_daftar" type="text" value="{{ $peserta[0]->created_at }}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                
-              </div>
-  
-              <div class="mb-3 col-3">
-                <label for="exampleInputEmail1" class="form-label">Nomor Tampil</label>
-                <input name="nomor_tampil" type="text" value="{{$peserta[0]->no_tampil}}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-              </div>
+        <div class="mb-3 col-3">
+            <label for="exampleInputEmail1" class="form-label">Lagu Pilihan</label>
+            <input name="lagu_pilihan" type="text" value="{{ $peserta[0]->judul_lagu }}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
 
-            </div>
-           
-            <button type="submit" class="btn btn-success">Simpan</button>
-          </form>
+        <div class="mb-3 col-3">
+            <label for="exampleInputEmail1" class="form-label">Kategori Lomba</label>
+            <input name="kategori_lomba" type="text" value="{{ $peserta[0]->kategori_lomba }}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+    </div>
+
+    <div class="horizontal">
+        <div class="mb-3 col-9">
+            <label for="exampleInputEmail1" class="form-label">Tanggal Pendaftaran</label>
+            <input disabled name="tgl_daftar" type="text" value="{{ $peserta[0]->created_at }}" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+
+        <div class="mb-3 col-3">
+            <label for="exampleInputEmail1" class="form-label">Nomor Tampil</label>
+            <input name="nomor_tampil" type="text" value="" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+    </div>
+</form>
+
         </div>
       </div>
 
