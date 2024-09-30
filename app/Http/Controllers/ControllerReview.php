fb6f7be7ca69f226f9c29237508bbfb3313dea6f
samberi.stenly <samberi.stenly@gmail.com>
2024-09-30 12:49:00 +0000

    nama juri

--- app/Http/Controllers/ControllerReview.php
+++ app/Http/Controllers/ControllerReview.php
@@ -164,6 +164,7 @@ class ControllerReview extends Controller
                 'medali'  => $medali,
                 'nomor_tampil' => $mappedReviews->first()['data']->first()->no_tampil,
                 'jemaat'  => $mappedReviews->first()['data']->first()->jemaat->nama,
+                'juri'  => $mappedReviews->first()['data']->first()->user->name,
                 'total_final' => $nilai_akhir
             ];
         });
