d3b2f0b3ab55c682f979eaa0e341ceda1303478b
samberi.stenly <samberi.stenly@gmail.com>
2024-10-01 18:53:51 +0000

    view setting data

--- resources/views/html/dash_setting.blade.php
+++ resources/views/html/dash_setting.blade.php
@@ -4,15 +4,8 @@
 <div class="card">
   <div class="card-body">
     <h5 class="card-title fw-semibold mb-4">Dash Setting</h5>
-    <div class="card mb-0">
-      <div class="card-body">
-        
-        <div class="row" id="category-container">
-      
-        </div>
-       
-      </div>
-     
+    <div class="card mb-0">   
+        <div class="row" id="category-container"></div>
     </div>
   </div>
 </div>
