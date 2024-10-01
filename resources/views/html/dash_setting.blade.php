bc15476dcb21813df6e073841b3696b270b42c69
samberi.stenly <samberi.stenly@gmail.com>
2024-10-01 18:29:12 +0000

    view setting dash

--- resources/views/html/dash_setting.blade.php
+++ resources/views/html/dash_setting.blade.php
@@ -6,36 +6,7 @@
     <h5 class="card-title fw-semibold mb-4">Dash Setting</h5>
     <div class="card mb-0">
       <div class="card-body">
-        <!-- <table class="table table-striped">
-          <thead>
-            <tr>
-              <th scope="col">No</th>
-              <th scope="col">Judul</th>
-              <th scope="col">Aksi</th>
-            </tr>
-          </thead>
-          <tbody>
-            @forelse ($lomba as $key => $lagu)
-            <tr>
-                <th scope="row">{{ $key + 1 }}</th>
-                <td>{{ $lagu->kategori_lomba }}</td>
-                <td>
-                    <form action="{{ route('dash.dash_updated', $lagu->id_kategori_lomba) }}" method="POST">
-                        @csrf
-                        @method('PUT')
-                        <button type="submit" class="btn btn-success">
-                          <i class="ti ti-trash"></i>
-                        </button>
-                    </form>
-                </td>
-            </tr>
-          @empty
-              <tr>
-                  <td colspan="5" class="text-center">Tidak ada data</td>
-              </tr>
-          @endforelse
-          </tbody>
-        </table> -->
+        
 
         <div class="row" id="category-container">
       
