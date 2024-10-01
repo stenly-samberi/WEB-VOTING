9ba7c30d6ff1d160596fd68385f0228eefb12130
samberi.stenly <samberi.stenly@gmail.com>
2024-10-01 19:40:02 +0000

    crft

--- resources/views/main/layout.blade.php
+++ resources/views/main/layout.blade.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>Modernize Free</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <link rel="shortcut icon" type="image/png" href="{{ asset('assets/images/logos/favicon.png') }}" />
