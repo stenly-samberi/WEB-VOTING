636ae65569b755215be7a4f7159e45363b2d2b12
samberi.stenly <samberi.stenly@gmail.com>
2024-10-02 15:01:49 +0000

    grup kategori

--- app/Http/Controllers/ControllerReview.php
+++ app/Http/Controllers/ControllerReview.php
@@ -171,7 +171,7 @@ class ControllerReview extends Controller
 
         return ['reviews' => $mappedReviews,
                 'medali'  => $medali,
-                'nomor_tampil' => $mappedReviews->first()['data']->first()->no_tampil,
+                // 'nomor_tampil' => $mappedReviews->first()['data']->first()->no_tampil,
                 'jemaat'  => $mappedReviews->first()['data']->first()->jemaat->nama,
                 'total_final' => $nilai_akhir
                 // 'juri'  => $juriData
