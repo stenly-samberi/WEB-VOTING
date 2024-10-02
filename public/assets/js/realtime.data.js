1347dfd684b3f1594c6de69aa66a8538f3eca98f
samberi.stenly <samberi.stenly@gmail.com>
2024-10-02 12:08:19 +0000

    log

--- public/assets/js/realtime.data.js
+++ public/assets/js/realtime.data.js
@@ -296,8 +296,25 @@ function updateStatus(id,isChecked) {
 
 function UpdateStatusViewDash(id, status) {
 
-    showErrorModal(id, status);
+    console.log(id, status);
 
+    // $.ajax({
+    //     url: '/dash/setting/update/' + id,
+    //     method: 'PUT',
+    //     data: {
+    //         status: status ? 1 : 0,
+    //         _token: '{{ csrf_token() }}'
+    //     },
+
+    //     success: function(response) {
+    //         showErrorModal(response)
+    //     },
+
+    //     error: function(xhr, status, error) {
+    //         console.log(error);
+          
+    //     }
+    // });
 }
 
 
