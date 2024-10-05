26abfe22019b07fef8414d76f4079333662bdd18
samberi.stenly <samberi.stenly@gmail.com>
2024-10-05 20:30:46 +0000

    grup_kat

--- app/Http/Controllers/ControllerReview.php
+++ app/Http/Controllers/ControllerReview.php
@@ -135,7 +135,7 @@ class ControllerReview extends Controller
             $query->where('status', 1);
         })->get();
     
-        $groupedReviews = $reviews->groupBy(['kategori_lomba', 'id_njemaat']);
+        $groupedReviews = $reviews->groupBy(['id_kategori_lomba', 'id_njemaat']);
 
         return response()->json(['data' => $groupedReviews]);
     
